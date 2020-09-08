d02ace7f0d65bfc8b6bd5e18bcbc4e8803c4d0db
abhishesh <mishra.abhi9619@gmail.com>
2020-09-08 18:06:27 +0530

    code for uplad all file local to s3 bucket

--- upload.py
+++ upload.py
@@ -2,12 +2,13 @@ import boto3
 import os 
 
 def key_exists(mykey, mybucket):
-	ACCESS_KEY = 'xxxxxxxxxxxxxxxx'
-	SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	ACCESS_KEY = 'xxxxxxxxxxxxxxxxxx'
+	SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 	
 	s3_client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,
                         aws_secret_access_key=SECRET_KEY,
                         region_name='ap-south-1')
+	
 	response = s3_client.list_objects_v2(Bucket=mybucket, Prefix=mykey)
 	# print(response)
 	if response:
@@ -21,17 +22,23 @@ def key_exists(mykey, mybucket):
 			if mykey == obj['Key']:
 				return True
 		return False
-
-# if key_exists('uploads/999239833temp.jpg', 'sathio-production'):
-#     print("key exists")
-# else:
-#     print("safe to put new bucket object")
-
-
 for file in os.listdir('media/'):
 	file_name = 'media/'+file
 	print(file_name)
-	if key_exists(file_name,'pro-sathio-bucket'):
+	if key_exists(file_name,'production-bucket'):
 		print("key exists")
 	else:
-		print("safe to put new bucket object")
+		base_path = 'media/'
+		# counter = 0
+		for file in os.listdir(base_path):
+			print(file)
+			file_name = base_path+'/'+file
+			ACCESS_KEY = 'xxxxxxxxxxxxxx'
+			SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+			s3_client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,
+                        aws_secret_access_key=SECRET_KEY,
+                        region_name='ap-south-1')
+			s3_client.upload_file(file_name, 'production-bucket', 'media/{}'.format(file))
+			print("successfully uploaded ")
+
+    
